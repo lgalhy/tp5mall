82220e715ef41b55ad73cf188a9d49f142e5a868
lgalhy <806408542@qq.com>
2017-04-07 22:11:10 +0800

    提交appli目录

---
+++ application/admin/common.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/6
+ * Time: 20:30
+ */
---
+++ application/index/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/6
+ * Time: 20:30
+ */
+return [
+    'template' =>[
+        'tpl_cache' => false, // 是否开启模板编译缓存,设为false则每次都会重新编译
+        'tpl_replace_string' => [
+            'index_public' => './public/'//自定义后台页面的css文件目录
+        ],
+    ],
+
+
+]
+?>
